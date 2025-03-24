7dd06a4818047d330ce95597fb150edd891bca0b
Jon <jseitzer83@gmail.com>
2025-03-23 20:46:27 -0400

    fix: remove deprecated swcMinify option

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   output: 'standalone',
   experimental: {
     // Enable React 19 features
