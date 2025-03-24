be9814dd2ea0b1624bfca68592a1f39acf6df0cc
Jon <jseitzer83@gmail.com>
2025-03-23 20:34:17 -0400

    chore: upgrade to Next.js 15.1 and React 19

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
+  experimental: {
+    // Enable React 19 features
+    serverActions: true,
+    typedRoutes: true,
+  }
 };
 
 module.exports = nextConfig;
